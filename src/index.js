3c64fd7013ac53624c3ba2a5f0184fe57fdef0e1
Dave Nash <davidjnash2@gmail.com>
2023-06-07 00:12:11 -0500

    installed dependencies, started form elements on appjsx, and on indexjs imported redux and provider and logger, added provider tags to render, and built airlineList reducer

--- src/index.js
+++ src/index.js
@@ -2,11 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
 
-/** TODO: import REDUX **/
+/** DONE HERE; TODO: import REDUX **/
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
 
 
 /** TODO: Add REDUCERS */
-
+const airlineList = (state = [], action) => {
+    console.log('in airlineList reducer');
+    if (action.type === 'ADD_AIRLINE'){
+        console.log(`confirm that action.payload is ${action.payload}`);
+        return [...state, action.payload]
+    }
+    
+    return state;
+}
 
 /** TODO: Create store */
 
@@ -16,6 +27,8 @@ import App from './components/App/App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <Provider store={storeInstance}>
+            <App />
+        </Provider>
     </React.StrictMode>
 );
